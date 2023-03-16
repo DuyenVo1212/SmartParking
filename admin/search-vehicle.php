a1281d45666f81ce8dcce7e05502e186962e16c0
ChucDuyen <songthuchet@gmail.com>
2023-03-16 20:08:19 +0700

    fix search

--- admin/search-vehicle.php
+++ admin/search-vehicle.php
@@ -12,10 +12,11 @@ if (strlen($_SESSION['vpmsaid']==0)) {
 <!doctype html>
 
 <html class="no-js" lang="">
+
 <head>
-   
+
     <title>VPMS - Search Vehicle</title>
-   
+
 
     <link rel="apple-touch-icon" href="https://i.imgur.com/QRAUqs9.png">
     <link rel="shortcut icon" href="https://i.imgur.com/QRAUqs9.png">
@@ -24,221 +25,57 @@ if (strlen($_SESSION['vpmsaid']==0)) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/lykmapipo/themify-icons@0.1.2/css/themify-icons.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
+    <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.0/css/flag-icon.min.css">
- 
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter a username.";
-    } else{
-        // Prepare a select statement
-        $sql = "SELECT id FROM users WHERE username = ?";
-
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("s", $param_username);
-
-            // Set parameters
-            $param_username = trim($_POST["username"]);
-
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Store result
-                $stmt->store_result();
-
-                if($stmt->num_rows == 1){
-                    $username_err = "This username is already taken.";
-                } else{
-                    $username = trim($_POST["username"]);
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            $stmt->close();
-        }
-    }
-
-    // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
-        $password_err = "Password must have at least 6 characters.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
-        $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
-            $confirm_password_err = "Password did not match.";
-        }
-    }
-
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
-
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("ss", $param_username, $param_password);
-
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-
-            // Attempt to execute the prepared statement
-            if($stmt->execute()){
-                // Redirect to login page
-                header("location: login.php");
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            $stmt->close();
-        }
-    }
-
-    // Close connection
-    $mysqli->close();
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Up</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <style type="text/css">
-        body{ font: 14px; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
-</head>
-<body>
-    <div class="wrapper">
-        <h2>Sign Up</h2>
-        <p>Please fill this form to create an account.</p>
-        <form action="<?php
-
-
-
-vochucduyeniuh@gmail.com
-continue
-login.php:
-
-php
-Copy code
-<?php
-// Initialize the session
-session_start();
-
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: welcome.php");
-    exit;
-}
-
-// Include config file
-require_once "config.php";
-
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = "";
-
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-
-        if($stmt = $mysqli->prepare($sql)){
-            // Bind variables to the prepared statement as parameters
-            $stmt->bind
-
-
-
-vochucduyeniuh@gmail.com
-write for me file code php:admin will manage the reservation list along with the location of the vehicle from the ir sensor and there is a license plate display obtained from the license plate recognition data of the previous raspberry camera
-admin.php:
-
-php
-Copy code
-<?php
-// Initialize the session
-session_start();
-
-// Check if the user is logged in, if not then redirect him to
-
-Stop generating   <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+    <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
     <link rel="stylesheet" href="assets/css/style.css">
 
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
 
 </head>
+
 <body>
     <!-- Left Panel -->
 
-  <?php include_once('includes/sidebar.php');?>
+    <?php include_once('includes/sidebar.php');?>
 
     <!-- Left Panel -->
 
     <!-- Right Panel -->
 
-     <?php include_once('includes/header.php');?>
+    <?php include_once('includes/header.php');?>
 
-        <div class="breadcrumbs">
-            <div class="breadcrumbs-inner">
-                <div class="row m-0">
-                    <div class="col-sm-4">
-                        <div class="page-header float-left">
-                            <div class="page-title">
-                                <h1>Dashboard</h1>
-                            </div>
+    <div class="breadcrumbs">
+        <div class="breadcrumbs-inner">
+            <div class="row m-0">
+                <div class="col-sm-4">
+                    <div class="page-header float-left">
+                        <div class="page-title">
+                            <h1>Dashboard</h1>
                         </div>
                     </div>
-                    <div class="col-sm-8">
-                        <div class="page-header float-right">
-                            <div class="page-title">
-                                <ol class="breadcrumb text-right">
-                                    <li><a href="dashboard.php">Dashboard</a></li>
-                                    <li><a href="search-vehicle.php">Search Vehicle</a></li>
-                                    <li class="active">Search Vehicle</li>
-                                </ol>
-                            </div>
+                </div>
+                <div class="col-sm-8">
+                    <div class="page-header float-right">
+                        <div class="page-title">
+                            <ol class="breadcrumb text-right">
+                                <li><a href="dashboard.php">Dashboard</a></li>
+                                <li><a href="search-vehicle.php">Search Vehicle</a></li>
+                                <li class="active">Search Vehicle</li>
+                            </ol>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="content">
-            <div class="animated fadeIn">
-                <div class="row">
-                   
-         
+    <div class="content">
+        <div class="animated fadeIn">
+            <div class="row">
+
+
 
                 <div class="col-lg-12">
                     <div class="card">
@@ -246,42 +83,48 @@ Stop generating   <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
                             <strong class="card-title">Search Vehicle</strong>
                         </div>
                         <div class="card-body">
-<form action="" method="post" enctype="multipart/form-data" class="form-horizontal" name="search">
-                                    
-                                   
-                                    <div class="row form-group">
-                                        <div class="col col-md-3"><label for="text-input" class=" form-control-label">Search By Parking Number</label></div>
-                                        <div class="col-12 col-md-9"><input type="text" id="searchdata" name="searchdata" class="form-control"  required="required" autofocus="autofocus" ></div>
-                                    </div>
-                                 
-                                    
-                                    
-                                   <p style="text-align: center;"> <button type="submit" class="btn btn-primary btn-sm" name="search" >Search</button></p>
-                                </form>
+                            <form action="" method="post" enctype="multipart/form-data" class="form-horizontal"
+                                name="search">
+                                <p style="font-size:16px; color:red" align="center"> <?php if($msg){
+    echo $msg;
+  }  ?> </p>
 
- <?php
+                                <div class="row form-group">
+                                    <div class="col col-md-3"><label for="text-input" class=" form-control-label">Search
+                                            By Parking Number</label></div>
+                                    <div class="col-12 col-md-9"><input type="text" id="searchdata" name="searchdata"
+                                            class="form-control" required="required" autofocus="autofocus"></div>
+                                </div>
+
+
+
+                                <p style="text-align: center;"> <button type="submit" class="btn btn-primary btn-sm"
+                                        name="search">Search</button></p>
+                            </form>
+
+                            <?php
 if(isset($_POST['search']))
 { 
 
 $sdata=$_POST['searchdata'];
   ?>
-  <h4 align="center">Result against "<?php echo $sdata;?>" keyword </h4> 
-                             <table class="table">
-                <thead>
-                                        <tr>
-                                            <tr>
-                  <th>S.NO</th>
-            
-                 
-                    <th>Parking Number</th>
-                    <th>Owner Name</th>
-                    <th>Vehicle Reg. Number</th>
-                   
-                          <th>Action</th>
-                </tr>
-                                        </tr>
-                                        </thead>
-               <?php
+                            <h4 align="center">Result against "<?php echo $sdata;?>" keyword </h4>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                    <tr>
+                                        <th>S.NO</th>
+
+
+                                        <th>Parking Number</th>
+                                        <th>Owner Name</th>
+                                        <th>Vehicle Reg. Number</th>
+
+                                        <th>Action</th>
+                                    </tr>
+                                    </tr>
+                                </thead>
+                                <?php
 $ret=mysqli_query($con,"select *from   tblvehicle where ParkingNumber like '$sdata%'");
 $num=mysqli_num_rows($ret);
 if($num>0){
@@ -289,54 +132,57 @@ $cnt=1;
 while ($row=mysqli_fetch_array($ret)) {
 
 ?>
-              
-                <tr>
-                  <td><?php echo $cnt;?></td>
-            
-                 
-                  <td><?php  echo $row['ParkingNumber'];?></td>
-                  <td><?php  echo $row['OwnerName'];?></td>
-                  <td><?php  echo $row['RegistrationNumber'];?></td>
-                  
-                  <td><a href="view-incomingvehicle-detail.php?viewid=<?php echo $row['ID'];?>">View</a></td>
-                </tr>
-                <?php 
+
+                                <tr>
+                                    <td><?php echo $cnt;?></td>
+
+
+                                    <td><?php  echo $row['ParkingNumber'];?></td>
+                                    <td><?php  echo $row['OwnerName'];?></td>
+                                    <td><?php  echo $row['RegistrationNumber'];?></td>
+
+                                    <td><a
+                                            href="view-incomingvehicle-detail.php?viewid=<?php echo $row['ID'];?>">View</a>
+                                    </td>
+                                </tr>
+                                <?php 
 $cnt=$cnt+1;
 } } else { ?>
-     <tr>
-    <td colspan="8"> No record found against this search</td>
+                                <tr>
+                                    <td colspan="8"> No record found against this search</td>
 
-  </tr>
-   
-<?php } }?>
-              </table>
+                                </tr>
 
+                                <?php } }?>
+                            </table>
+
+                        </div>
                     </div>
                 </div>
+
+
+
             </div>
+        </div><!-- .animated -->
+    </div><!-- .content -->
 
+    <div class="clearfix"></div>
 
+    <?php include_once('includes/footer.php');?>
 
-        </div>
-    </div><!-- .animated -->
-</div><!-- .content -->
+    </div><!-- /#right-panel -->
 
-<div class="clearfix"></div>
+    <!-- Right Panel -->
 
-<?php include_once('includes/footer.php');?>
-
-</div><!-- /#right-panel -->
-
-<!-- Right Panel -->
-
-<!-- Scripts -->
-<script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
-<script src="assets/js/main.js"></script>
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@2.2.4/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.4/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery-match-height@0.7.2/dist/jquery.matchHeight.min.js"></script>
+    <script src="assets/js/main.js"></script>
 
 
 </body>
+
 </html>
 <?php }  ?>
